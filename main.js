58b98766bc484ed4d1866bf338f903e4a958d4e6
esdb1 <pierre.lacognata@mail-esd.com>
2023-10-17 15:58:40 +0200

    added some logic

--- main.js
+++ main.js
@@ -2,10 +2,24 @@
 
 const landing = document.querySelector('.landing')
 
-let backgrounds = ["bgCocotte", "bgBocaux", "bgPoelleFonte"]
+let backgrounds = ["bgCocotte", "bgBocaux", "bgPoeleFonte"]
 
-landing.classList.add(backgrounds[1])
-landing.classList.remove(backgrounds[1])
+setInterval(  change , 1000 )
+
+let index = 0
+function change(){
+
+
+    landing.classList.remove(backgrounds[index])
+
+    index++
+
+    landing.classList.add(backgrounds[index])
+
+
+}
+
+
 
 
 // faire en sorte que le background alterne parmis les trois disponibles
